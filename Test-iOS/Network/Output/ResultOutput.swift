48d0ff970eb00254a62471f3e17d857ff0277754
Rafael Damasceno <rafael.damaceno@cwi.com.br>
2019-07-26 15:23:10 -0300

    [FIX]: extension for double value for latitude and output decoder

--- Test-iOS/Network/Output/ResultOutput.swift
+++ Test-iOS/Network/Output/ResultOutput.swift
@@ -10,25 +10,59 @@ struct EventsOutput: Decodable {
     let id: String
     let title: String
     let price: Double
-    let latitude: Double
-    let longitude: Double
+    var latitude: LocationOutput
+    var longitude: LocationOutput
     let image: String
-    let resultOutputDescription: String
+    let description: String
     let date: Int
-    let people: [CuponOutput]
-    let cupons: [PersonOutput]
+    let cupons: [CuponOutput]
+    let people: [PeopleOutput]
+}
+
+fileprivate extension Double {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if let value = try? container.decode(Double.self) {
+            self = value
+        }
+        if let value = try? container.decode(String.self) {
+            if let value = Double(value) {
+                self = value
+            }
+        }
+        throw DecodingError.typeMismatch(LocationOutput.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for IntOrString"))
+    }
+}
+
+enum  LocationOutput: Decodable {
+    case double(Double)
+    case string(String)
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let value = try? container.decode(Double.self) {
+            self = .double(value)
+            return
+        }
+        if let value = try? container.decode(String.self) {
+            self = .string(value)
+            return
+        }
+        throw DecodingError.typeMismatch(LocationOutput.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for IntOrString"))
+    }
 }
 
 struct CuponOutput: Decodable {
-    let id: String
-    let eventID: String
-    let discount: Int
+    let id: String?
+    let eventID: String?
+    let discount: Int?
 }
 
-struct PersonOutput: Decodable {
-    let id: String
-    let eventID: String
-    let name: String
-    let picture: String
+struct PeopleOutput: Decodable {
+    let id: String?
+    let eventID: String?
+    let name: String?
+    let picture: String?
 }
 
